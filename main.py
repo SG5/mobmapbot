d596841a92fbb3c55d0f18922303b2b4e847fd4c
SG5 <sergeyv89@gmail.com>
2016-10-03 15:27:40 +0300

    init

---
+++ main.py
@@ -0,0 +1,32 @@
+from flask import Flask
+import mobile_bot as bot
+
+app = Flask(__name__)
+bot.send_webhook_url()
+
+
+@app.route('/')
+def hello():
+    return 'Hello World'
+
+
+@app.route('/' + bot.get_webhook_url(), methods=['POST'])
+def webhook_handler():
+    return bot.webhook_handler()
+
+
+@app.route('/3g/tele2', methods=['GET'])
+def tele2Handler():
+    return bot.tele2()
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    """Return a custom 404 error."""
+    return 'Sorry, Nothing at this URL.', 404
+
+
+@app.errorhandler(500)
+def application_error(e):
+    """Return a custom 500 error."""
+    return 'Sorry, unexpected error: {}'.format(e), 500
---
+++ mobile_bot.py
@@ -0,0 +1,50 @@
+import urllib2
+
+from google.appengine.api import app_identity
+from hashlib import sha256
+from flask import request
+from secret import get_bot_token as token
+import telegram
+
+bot = telegram.Bot(token=token())
+WEB_HOOK_URL = sha256(token()).hexdigest()
+
+
+def webhook_handler():
+    # retrieve the message in JSON and then transform it to Telegram object
+    update = telegram.Update.de_json(request.get_json(force=True))
+
+    if not update.message:
+        return 'ok'
+
+    chat_id = update.message.chat.id
+
+    # Telegram understands UTF-8, so encode text for unicode compatibility
+    if update.message.text:
+        bot.sendMessage(chat_id=chat_id, text=chat_id)
+
+    return 'ok'
+
+
+def tele2():
+    try:
+        urllib2.urlopen('http://msk.tele2.ru/CoverageData/regions/msk/_3g/12/2476/1292.png')
+        try:
+            urllib2.urlopen('http://msk.tele2.ru/CoverageData/regions/msk/_lte/12/2476/1292.png')
+            bot.sendMessage(chat_id='224473640', text='LTE work!')
+        except urllib2.HTTPError as e:
+            if 404 == e.code:
+                bot.sendMessage(chat_id='224473640', text='3g exist, LTE not found')
+    except urllib2.HTTPError as e:
+        if 404 == e.code:
+            bot.sendMessage(chat_id='224473640', text='3g not found')
+
+    return 'ok'
+
+
+def get_webhook_url():
+    return WEB_HOOK_URL
+
+
+def send_webhook_url():
+    bot.setWebhook(app_identity.get_default_version_hostname() + "/" + WEB_HOOK_URL)
