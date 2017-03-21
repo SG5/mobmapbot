28d50c226f6bbca694ec225e82fc3e3728db9bc7
SG5 <sergeyv89@gmail.com>
2017-03-21 16:01:14 +0300

    return ok code and change date at misc

--- misc.py
+++ misc.py
@@ -7,13 +7,11 @@ CHAT_ID = '224473640'
 
 
 def rfpl():
-    values = {"ajaxAction": "getTourStats","tour": 2327,"season": 685,}
-
-    values = urllib.urlencode(values)
+    values = urllib.urlencode({"ajaxAction": "getTourStats","tour": 2328,"season": 685,})
     req = urllib2.Request('http://rfpl.org/ajax/match/', values)
     data = json.load(urllib2.urlopen(req))
 
-    if u'Дата уточняется' != data['contents'][0]['date']:
+    if u'Дата уточняется' != data['contents'][6]['date']:
         bot.sendMessage(chat_id=CHAT_ID, text='rfpl: calendar change!')
 
     return 'ok'
@@ -21,5 +19,6 @@ def rfpl():
 
 def velobike():
     response = urllib2.urlopen('http://velobike.ru/news/')
-    if -1774534778 != binascii.crc32(response.read()):
+    if (-1774534778) != binascii.crc32(response.read()):
         bot.sendMessage(chat_id=CHAT_ID, text='velobike: news updated!')
+    return 'ok'
